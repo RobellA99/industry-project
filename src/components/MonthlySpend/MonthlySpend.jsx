cc26589e84a33eacb2467e038ef78c2f8a39f75f
Robell <robellasfaw@gmail.com>
2025-03-28 01:11:08 +0000

    Initial Commit

---
+++ src/components/MonthlySpend/MonthlySpend.jsx
@@ -0,0 +1,11 @@
+export default function MonthlySpend() {
+  return (
+    <article>
+      <h2>Monthly Spend</h2>
+      <div>
+        <h3>£350.00</h3>
+        <p>£50.00</p>
+      </div>
+    </article>
+  );
+}
